b1b18e56f35569b10326fce04971e3f6c92ce572
Daniel Muller <daniel.muller034@gmail.com>
2023-01-06 14:18:05 -0700

    Assesment/BouncingBalls - aligned function lines to tidy things up

--- Assesment/Bouncing-Balls/main-finished.js
+++ Assesment/Bouncing-Balls/main-finished.js
@@ -109,7 +109,7 @@ class Ball extends Shape {
       ctx.fillStyle = this.color;
       ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
       ctx.fill();
-  }
+    }
 
     // changes velocity of ball to prevent the ball going off the
     // page
@@ -132,14 +132,14 @@ class Ball extends Shape {
       
       this.x += this.velX;
       this.y += this.velY;
-      }
+    }
 
     // check area of the current ball being looped through
     // against balls in the array to see if they overlap,
     // if so, change color
-      collisionDetect() {
+    collisionDetect() {
         for (const ball of balls) {
-          if (!(this === ball) && ball.exists) {
+        if (!(this === ball) && ball.exists) {
             const dx = this.x - ball.x;
             const dy = this.y - ball.y;
             const distance = Math.sqrt(dx * dx + dy * dy);
@@ -149,49 +149,50 @@ class Ball extends Shape {
             }
           }
         }
-      }
-
     }
 
-    // storing balls
-    const balls = [];
+}
 
-    // create new balls loop
-    while (balls.length < 25) {
-      const size = random(10, 20);
-      const ball = new Ball(
-        // ball position always drawn at least one ball width
-        // away from the edge of the canvas to avoid drawing
-        // errors
-        random(0 + size, width - size),
-        random(0 + size, height - size),
-        random(-7, 7),
-        random(-7, 7),
-        randomRGB(),
-        size
-      );
+// storing balls
+const balls = [];
 
-      balls.push(ball);
-    }
+// create new balls loop
+while (balls.length < 25) {
+    const size = random(10, 20);
+    const ball = new Ball(
+    // ball position always drawn at least one ball width
+     // away from the edge of the canvas to avoid drawing
+    // errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+    );
+
+    balls.push(ball);
+}
 
   
- // recursively calls loop function via requsetAnimationFrame
- // (loop) method
-    function loop() {
-      // set canvas color and add transparency to allow ball trails via 4th argument in fillStyle
-      ctx.fillStyle = "rgba(0,0,0,0.25)";
-      ctx.fillRect(0,0, width, height);
+// recursively calls loop function via requsetAnimationFrame
+// (loop) method
+function loop() {
+    // set canvas color and add transparency to allow ball trails via 4th argument in fillStyle
+    ctx.fillStyle = "rgba(0,0,0,0.25)";
+    ctx.fillRect(0,0, width, height);
 
     // loop through balls array to draw ball in a new place,
     // adjust velocity for the next frame,
     // check if balls collide
-      for (const ball of balls) {
+    for (const ball of balls) {
         ball.draw();
         ball.update();
         ball.collisionDetect();
-      }
-
-      requestAnimationFrame(loop);
     }
 
-    loop();
+
+    requestAnimationFrame(loop);
+}
+
+loop();
